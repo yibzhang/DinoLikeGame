d94b91643cd8dc8fa5e9ab038b24386df7342391
yibzhang <zhangyiboturku@gmail.com>
2021-01-03 00:34:25 +0200

    add random speed for enermy

--- script/index.js
+++ script/index.js
@@ -1,128 +1,120 @@
 var start_game = document.getElementById("start");
 var frame = document.getElementById("frame");
-var player = document.getElementById("player");
-var enermy = document.getElementById("enermy");
 
-var score_display = document.getElementById("score")
-var cloud = document.getElementById("cloud")
-var mountains = document.getElementById("mountains")
-var grass = document.getElementById("grass")
+var game_update_rate = 50;
 
-var game_update_rate = 100;
-var score = 0;
-
-function player_start_animation() {
-  player.style.backgroundImage = "url('images/player_horse_animated.gif')"
-}
-
-function player_stop_animation() {
-  player.style.backgroundImage = "url('images/player_horse_static.png')"
-}
-
-function player_jump() {
-  // Add player_jump class if it doesn't exist
-  if (!player.classList.contains("player_jump")) {
-    player.classList.add("player_jump");
-    setTimeout(function () {
-      player.classList.remove("player_jump");
-    }, 1000);
+var score = {
+  scoreObj: document.getElementById("score"),
+  score: 0,
+  add: function () {
+    this.score++;
+    this.scoreObj.innerHTML = "score : " + this.score;
+  },
+  reset: function () {
+    this.score = 0;
+    this.scoreObj.innerHTML = "score : 0";
   }
 }
 
-function collision_detection() {
-  // add some buffer to make the solid collide
-  var buffer_size = 5
-  var enermy_right = parseInt(enermy.getBoundingClientRect().right);
-  var enermy_left = parseInt(enermy.getBoundingClientRect().left);
-  var enermy_top = parseInt(enermy.getBoundingClientRect().top);
-  var player_right = parseInt(player.getBoundingClientRect().right);
-  var player_left = parseInt(player.getBoundingClientRect().left);
-  var player_bottom = parseInt(player.getBoundingClientRect().bottom);
-  return (
-    player_bottom >= enermy_top &&
-    enermy_left <= player_right - buffer_size &&
-    enermy_right >= player_left + buffer_size
-  );
-}
-
-function enermy_passed_player() {
-  return (parseInt(enermy.getBoundingClientRect().right) < parseInt(player.getBoundingClientRect().left));
-}
-
-function enermy_move() {
-  enermy.classList.add("enermy_move")
-}
-
-function enermy_stop() {
-  enermy.style.right = getComputedStyle(enermy).right
-  enermy.classList.remove("enermy_move")
-}
-
-function enermy_reset() {
-  enermy.style.right = "0px";
-  if (enermy.classList.contains('enermy_move')) {
-    enermy.classList.remove('enermy_move')
+var player = {
+  playerObj: document.getElementById("player"),
+  start: function () {
+    this.playerObj.style.backgroundImage = "url('images/player_horse_animated.gif')"
+  },
+  stop: function () {
+    this.playerObj.style.backgroundImage = "url('images/player_horse_static.png')"
+  },
+  jump: function () {
+    if (!this.playerObj.classList.contains("player_jump")) {
+      this.playerObj.classList.add("player_jump");
+      setTimeout(() => {
+        this.playerObj.classList.remove("player_jump")
+      }, 1000);
+    }
   }
 }
 
-function background_move() {
-  cloud.style.right = -cloud.clientWidth + "px"
-  cloud.classList.add('cloud_move')
-  grass.style.right = -grass.clientWidth + "px"
-  grass.classList.add('grass_move')
-  mountains.style.left = "0";
-  mountains.classList.add('mountains_move')
+var enermy = {
+  enermyObj: document.createElement("div"),
+  spawn: function () {
+    this.enermyObj.setAttribute("id", "enermy");
+    this.enermyObj.classList.add("enermy")
+    this.enermyObj.classList.add("enermy_move")
+    this.enermyObj.style.animationDuration = (Math.random() * 2 + 1.5) + "s";
+    frame.appendChild(this.enermyObj)
+  },
+  stop: function () {
+    this.enermyObj.style.right = getComputedStyle(this.enermyObj).right
+    this.enermyObj.classList.remove("enermy_move")
+  },
+  destory: function () {
+    this.enermyObj.remove()
+  }
 }
 
-function background_stop() {
-  cloud.style.right = getComputedStyle(cloud).right;
-  cloud.classList.remove('cloud_move')
-  grass.style.right = getComputedStyle(grass).right;
-  grass.classList.remove('grass_move')
-  mountains.style.left = getComputedStyle(mountains).left;
-  mountains.classList.remove('mountains_move')
+var background = {
+  cloudObj: document.getElementById("cloud"),
+  mountainsObj: document.getElementById("mountains"),
+  grassObj: document.getElementById("grass"),
+  start: function () {
+    this.cloudObj.style.right = -this.cloudObj.clientWidth + "px"
+    this.cloudObj.classList.add('cloud_move')
+    this.grassObj.style.right = -this.grassObj.clientWidth + "px"
+    this.grassObj.classList.add('grass_move')
+    this.mountainsObj.style.left = "0";
+    this.mountainsObj.classList.add('mountains_move')
+  },
+  stop: function () {
+    this.cloudObj.style.right = getComputedStyle(this.cloudObj).right;
+    this.cloudObj.classList.remove('cloud_move')
+    this.grassObj.style.right = getComputedStyle(this.grassObj).right;
+    this.grassObj.classList.remove('grass_move')
+    this.mountainsObj.style.left = getComputedStyle(this.mountainsObj).left;
+    this.mountainsObj.classList.remove('mountains_move')
+  }
 }
 
-function add_score() {
-  score++;
-  score_display.innerHTML = "score : " + score;
+function isCollided(object1, object2) {
+  var object_1 = object1.getBoundingClientRect();
+  var object_2 = object2.getBoundingClientRect();
+
+  return (object_1.left < object_2.left + object_2.width && object_1.left + object_1.width > object_2.left &&
+    object_1.top < object_2.top + object_2.height && object_1.top + object_1.height > object_2.top)
 }
 
-function reset_score() {
-  score = 0;
-  score_display.innerHTML = "score : " + score;
+function playerJump(){
+  player.jump();
 }
 
 function game_over() {
   start_game.style.display = "block";
-  frame.removeEventListener("click", player_jump, true);
-  enermy_stop()
-  background_stop()
-  player_stop_animation()
+  frame.removeEventListener("click", playerJump, true);
+  player.stop()
+  enermy.stop()
+  background.stop()
 }
 
 function game_start() {
-  enermy_reset()
-  reset_score()
-  player_start_animation()
+  score.reset()
+  enermy.spawn()
+  player.start()
+  background.start()
+
   start_game.style.display = 'none'
-  frame.addEventListener('click', player_jump, true)
+  frame.addEventListener('click', playerJump, true)
 
-  enermy_move()
-  background_move()
+  game_update_interval = setInterval(() => {
 
-  score_added = false;
-
-  game_update_interval = setInterval(game_update, game_update_rate)
-  function game_update() {
-    if (collision_detection()) {
+    if (isCollided(player.playerObj, enermy.enermyObj)) {
       game_over()
-      clearInterval(game_update_interval);
-    } else {
-      if (enermy_passed_player() && !score_added) { add_score(); score_added = true }
-      if (!enermy_passed_player() && score_added) { score_added = false }
     }
-  }
+
+    if (enermy.enermyObj.offsetLeft < 0) {
+      enermy.destory()
+      enermy.spawn()
+      score.add()
+    }
+  }, game_update_rate)
 }
 
 start_game.addEventListener("click", game_start);
